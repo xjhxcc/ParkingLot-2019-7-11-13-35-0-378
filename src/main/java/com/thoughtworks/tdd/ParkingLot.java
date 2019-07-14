d60a8c7c93bbf213996e8e13221ac47ba0be5888
xjhxcc <2351889342@qq.com>
2019-07-14 14:27:12 +0800

    refactor:story 1 ac2 parkingBoy fetch cars by tickets

--- src/main/java/com/thoughtworks/tdd/ParkingLot.java
+++ src/main/java/com/thoughtworks/tdd/ParkingLot.java
@@ -1,12 +1,23 @@
 package com.thoughtworks.tdd;
 public class ParkingLot {
-    private Car car;
+    private Car car1;
+    private Car car2;
     public Ticket park(Car car) {
-        this.car=car;
+        if(car1==null){
+            car1=car;
+        }else{
+            car2=car;
+        }
         return new Ticket();
     }
 
     public Car fetch(Ticket ticket) {
-        return car;
+        if(car1!=null){
+            Car tempcar=car1;
+            car1=null;
+            return tempcar;
+        }else{
+            return car2;
+        }
     }
 }
--- src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class ParkingBoyTest {
     @Test
-    public void should_return_ticket_when_call_fetch_given_ticket_from_park(){
+    public void should_return_car_when_call_fetch_given_ticket_from_park(){
         //given
         ParkingLot parkingLot=new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
@@ -15,5 +15,21 @@ public class ParkingBoyTest {
         //then
         Assertions.assertSame(car,fetchCar);
 
+    }
+    @Test
+    public void should_return_cars_when_call_fetch_car_given_tickets_by_parking_car(){
+        //given
+        ParkingLot parkingLot=new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        Ticket ticket2 = parkingBoy.park(car2);
+        //when
+        Car fetchCar1 = parkingBoy.fetch(ticket1);
+        Car fetchCar2 = parkingBoy.fetch(ticket2);
+        //then
+        Assertions.assertSame(car1,fetchCar1);
+        Assertions.assertSame(car2,fetchCar2);
     }
 }
